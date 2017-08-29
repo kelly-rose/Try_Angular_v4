e894e7027c924061635dac0b1151d3865784b16c
Kelly <kellyrose.evergreen@gmail.com>
2017-08-29 21:26:04 +0900

    18 ngForm Basics

--- src/app/search/search.component.ts
+++ src/app/search/search.component.ts
@@ -6,10 +6,19 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./search.component.css']
 })
 export class SearchComponent implements OnInit {
-
+ searchLocation = "newport Beach";
+  // searchQuery ="searchQueryTT";
   constructor() { }
 
   ngOnInit() {
+  }
+
+  submitSearch(event,formData){
+    console.log(event);
+    console.log(formData.value);
+  }
+  searchQueryChange(){
+    this.searchLocation='California';
   }
 
 }
